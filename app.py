f6199618318e68621394dd3e430f669e633ae443
Hiimus <hiimhanseneirik@gmail.com>
2021-08-19 08:22:44 +0000

    Add profile page with session user profile

--- app.py
+++ app.py
@@ -47,6 +47,8 @@ def register():
         # put the new user into 'session' cookie
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful!")
+        return redirect(url_for("profile", username=session["user"]))
+
     return render_template("register.html")
 
 
@@ -63,7 +65,10 @@ def login():
             if check_password_hash(
                 existing_user["password"], request.form.get("password")):
                     session["user"] = request.form.get("username").lower()
-                    flash("Welcome, {}".format(request.form.get("username")))
+                    flash("Welcome, {}".format(
+                        request.form.get("username")))
+                    return redirect(url_for(
+                        "profile", username=session["user"]))
             else:
                 # password does not match
                 flash("Incorrect Username and/or Password")
@@ -75,6 +80,14 @@ def login():
             return redirect(url_for("login"))
 
     return render_template("login.html")
+
+
+# FROM CI VIDEOS
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    # uses the session user's username from database
+    username = mongo.db.users.find_one({"username":session["user"]})["username"]
+    return render_template("profile.html", username=username)
 
 
 if __name__ == "__main__":
