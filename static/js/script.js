74241287d5d46bdfdd3cdbe5685a8a9ced6c5fe1
Hiimus <hiimhanseneirik@gmail.com>
2021-08-20 09:43:01 +0000

    Added a template form for the adding recipe page.

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
+    $('.collapsible').collapsible();
+
+    var elem = document.querySelector('.collapsible.expandable');
+    var instance = M.Collapsible.init(elem, {
+    accordion: false
+    });
   });
+
+  
