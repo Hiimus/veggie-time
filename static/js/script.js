3ea47ed4d064455194f1e3f4159f599f6e4340d7
Hiimus <hiimhanseneirik@gmail.com>
2021-08-20 14:13:03 +0000

    Removed an add ingredient button and styled delete button

--- static/js/script.js
+++ static/js/script.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     $('.collapsible').collapsible();
 
     // Credit to https://www.sanwebcorner.com/2017/02/dynamically-generate-form-fields-using.html for adding new field to ingredients
-    
+
     var max_fields      = 10;
     var wrapper         = $(".container1");
     var add_button      = $(".add_form_field");
@@ -13,7 +13,7 @@ $(document).ready(function(){
         e.preventDefault();
         if(x < max_fields){
             x++;
-            $(wrapper).append("<div><input id='ingredients' name='ingredients' type='text' maxlength='100' class='validate_me'><label for='ingredients'>Ingredients</label><a href='#' class='delete'>Delete</a></div>"); //add input box
+            $(wrapper).append("<div class='input-field col s12 m8 l8'><input id='ingredients' name='ingredients' type='text' maxlength='100' class='validate_me'><label for='ingredients'>Ingredients</label><button type='button' class='delete btn-floating btn-small waves-effect waves-light red'><i class='fas fa-minus prefix'></i></button></div>"); //add input box
         }
   else
   {
