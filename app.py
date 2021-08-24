ac9f0ab9488271048017fddc09b00e3ff64f1452
Hiimus <hiimhanseneirik@gmail.com>
2021-08-24 09:08:03 +0000

    Added a delete button that deletes recipe

--- app.py
+++ app.py
@@ -113,7 +113,7 @@ def add_recipe():
             "recipe_name": request.form.get("recipe_name"),
             "recipe_description": request.form.get("recipe_description"),
             "difficulty_name": request.form.get("difficulty_name"),
-            "ingredients": request.form.getlist("ingredients"),
+            "ingredients": request.form["ingredients"],
             "instructions": request.form.getlist("instructions"),
             "created_by": session["user"]
         }
@@ -148,6 +148,14 @@ def edit_recipe(recipe_id):
     difficulties = mongo.db.difficulties.find()
     return render_template(
         "edit_recipes.html", recipe=recipe, categories=categories, difficulties=difficulties)
+
+
+# Credit to: https://learn.codeinstitute.net/courses/course-v1:CodeInstitute+DCP101+2017_T3/courseware/9e2f12f5584e48acb3c29e9b0d7cc4fe/1f324afea21e4bcabb6db2fcf3d16aab/?child=first
+@app.route("/delete_recipe/<recipe_id>")
+def delete_recipe(recipe_id):
+    mongo.db.recipes.remove({"_id": ObjectId(recipe_id)})
+    flash("Recipe Successfully Deleted")
+    return redirect(url_for("get_recipes"))
 
 
 if __name__ == "__main__":
