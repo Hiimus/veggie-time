4b2a70113db44507568d41f27d847ed4096ea4ba
Hiimus <hiimhanseneirik@gmail.com>
2021-08-29 10:36:58 +0000

    Fixed remove icon on add ingredient

--- static/js/script.js
+++ static/js/script.js
@@ -7,16 +7,21 @@ $(document).ready(function(){
 
     // Credit to https://www.sanwebcorner.com/2017/02/dynamically-generate-form-fields-using.html for adding new field to ingredients
 
-    var max_fields      = 10;
-    var wrapper         = $(".container1");
-    var add_button      = $(".add_form_field");
- 
-    var x = 1;
-    $(add_button).click(function(e){
+    let max_fields      = 10;
+    let wrapper         = $(".container1");
+    let add_button      = $(".add_form_field");
+    let x = 1;
+    let appendIngredient = `<div class='input-field col s12 m8 l8'>
+        <input id='ingredients' name='ingredients' type='text' maxlength='100' class='validate_me'>
+        <label for='ingredients'>Ingredients</label>
+        <button type='button' class='delete btn-floating btn-small waves-effect waves-light red'><i class='fas fa-minus'></i></button>
+    </div>`
+
+    $(add_button).click(function(e) {
         e.preventDefault();
-        if(x < max_fields){
+        if (x < max_fields) {
             x++;
-            $(wrapper).append("<div class='input-field col s12 m8 l8'><input id='ingredients' name='ingredients' type='text' maxlength='100' class='validate_me'><label for='ingredients'>Ingredients</label><button type='button' class='delete btn-floating btn-small waves-effect waves-light red'><i class='fas fa-minus prefix'></i></button></div>"); //add input box
+            $(wrapper).append(appendIngredient); //add input box
         }
   else
   {
